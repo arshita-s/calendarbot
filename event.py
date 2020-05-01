dae595e4c589c9c7f762f11ecbdbc19eca3e2c43
Arshita Sandhiparthi <arshita.237@gmail.com>
2020-05-01 16:48:50 -0700

    Testing event listener on heroku.

--- event.py
+++ event.py
@@ -10,11 +10,8 @@ from slack import RTMClient
 def event(**payload):
     data = payload['data']
     web_client = payload['web_client']
-    if "/cal" in data['text']:
+    if "event" in data['text']:
         channel_id = data['channel']
         thread_ts = data['ts']
         user = data['user']
         web_client.chat_postMessage(channel=channel_id, thread=thread_ts, text="Would you like to create an event, <@{user}>?")
-    slack_token = 'xoxb-1101498483268-1119164442736-5Bf1uHbCNZR7ClG82pqrIUXn'
-    rtm_client = RTMClient(token=slack_token)
-    rtm_client.start()
