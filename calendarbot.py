1f926afc3073717d4dbe0847a3f84b50af69497c
Arshita Sandhiparthi <arshita.237@gmail.com>
2020-04-30 23:07:17 -0700

    Some minor changes.

--- calendarbot.py
+++ calendarbot.py
@@ -1,9 +1,11 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
-import ssl
+
 import os
 from slack import WebClient
 from slack.errors import SlackApiError
+
+#slack_token = os.environ["SLACK_BOT_TOKEN"]
 
 slack_token = 'xoxb-1101498483268-1119164442736-5Bf1uHbCNZR7ClG82pqrIUXn'
 client = WebClient(token=slack_token)
@@ -11,8 +13,7 @@ client = WebClient(token=slack_token)
 try:
   response = client.chat_postMessage(
     channel="general",
-    text="Hello, this is @Calendarbot"
+    text="Hello, this is your @Calendarbot"
   )
 except SlackApiError as e:
-  # You will get a SlackApiError if "ok" is False
   assert e.response["error"]
