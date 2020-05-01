998e62481a8aa47e6e8b82b698884b8ecdadaf56
Arshita Sandhiparthi <arshita.237@gmail.com>
2020-04-30 21:50:49 -0700

    Calendar bot posts a random message to #general.

--- calendarbot.py
+++ calendarbot.py
@@ -1,19 +1,18 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import ssl
+import os
 from slack import WebClient
 from slack.errors import SlackApiError
-import os
-from slack import logging
 
-logging.basicConfig(level=logging.DEBUG)
-
-slack_token = os.environ["SLACK_API_TOKEN"]
+slack_token = 'xoxb-1101498483268-1119164442736-5Bf1uHbCNZR7ClG82pqrIUXn'
 client = WebClient(token=slack_token)
 
 try:
   response = client.chat_postMessage(
     channel="general",
-    text="Hello."
+    text="Hello, this is @Calendarbot"
   )
 except SlackApiError as e:
   # You will get a SlackApiError if "ok" is False
   assert e.response["error"]
-
