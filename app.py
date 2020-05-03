fa576d1b614df3c48b6518b656d7c176be26bafa
Arshita Sandhiparthi <arshita.237@gmail.com>
2020-05-03 14:13:10 -0700

    Slash command /event.

---
+++ app.py
@@ -0,0 +1,61 @@
+"""
+Arshita Sandhiparthi
+
+Main
+"""
+import logging
+import os
+import slack
+import event
+from flask import abort, Flask, jsonify, request
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main():
+    # slack_token = os.environ["SLACK_BOT_TOKEN"]
+
+    slack_token = 'xoxb-1101498483268-1105954847428-uwiOHt0WpJ42LjEXRHnZf5bf'
+    client = slack.WebClient(token=slack_token)
+    rtm_client = slack.RTMClient(token=slack_token, connect_method='rtm.start')
+
+    botID = None
+    users = client.api_call("users.list").get('members')
+    for user in users:
+        if user.get('name') == 'calendarbot':
+            botID = user.get('id')
+            break
+
+    response = client.chat_postMessage(channel="general", text="Hello, this is your " + get_mention(botID) + ". :calendar:")
+    rtm_client.start()
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
+def get_mention(user):
+    return '<@{user}>'.format(user=user)
+
+
+# Slash commands
+
+@app.route('/event', methods=['POST'])
+def event():
+    if not is_request_valid(request):
+        abort(400)
+
+    return jsonify(
+        response_type='in_channel',
+        text="Would you like to create an event?",
+    )
+
+
+def is_request_valid(request):
+    is_token_valid = request.form['token'] == 'lhbdgYshgpvXAtiqQ733M55e'
+    is_team_id_valid = request.form['team_id'] == 'U013H4UD0MN'
+
+    return is_token_valid and is_team_id_valid
+
+
+if __name__ == "__main__":
+    main()
--- event.py
+++ event.py
@@ -6,12 +6,10 @@ Events Handling
 
 from slack import RTMClient
 
+
 @RTMClient.run_on(event="message")
-def event(**payload):
+def msg_handler(**payload):
     data = payload['data']
     web_client = payload['web_client']
-    if "event" in data['text']:
-        channel_id = data['channel']
-        thread_ts = data['ts']
-        user = data['user']
-        web_client.chat_postMessage(channel=channel_id, thread=thread_ts, text="Would you like to create an event, <@{user}>?")
+
+
