dc7d50b456a92b25f3d4022b15cb62945dd672ba
Arshita Sandhiparthi <arshita.237@gmail.com>
2020-05-03 23:29:15 -0700

    /event slash command works locally. Added "make new event" button that opens a dialogue box.

--- app.py
+++ app.py
@@ -6,19 +6,21 @@ Main
 import logging
 import os
 import slack
-import event
-from bottle import run, post,request,response,route
-from urllib import parse
+from flask import Flask, request, make_response
+from slackeventsapi import SlackEventAdapter
+import json
 
 SLACK_VERIFICATION_TOKEN = 'lhbdgYshgpvXAtiqQ733M55e'
 SLACK_BOT_TOKEN = 'xoxb-1101498483268-1105954847428-uwiOHt0WpJ42LjEXRHnZf5bf'
+SLACK_SIGNING_SECRET = '6ee8492929d135bf1ba948350114e6f4'
 logging.basicConfig(level=logging.DEBUG)
+app = Flask(__name__)
 
 # initializing clients
 
 slack_token = SLACK_BOT_TOKEN
 client = slack.WebClient(token=slack_token)
-rtm_client = slack.RTMClient(token=slack_token, connect_method='rtm.start')
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
 
 
 # find the id of the bot
@@ -28,34 +30,102 @@ def get_mention(user):
 
 
 # greeting message from calendarbot
-
-botID = None
-users = client.api_call("users.list").get('members')
-for user in users:
-    if user.get('name') == 'calendarbot':
-        botID = user.get('id')
-        break
-
-response = client.chat_postMessage(channel="general", text="Hello, this is your " + get_mention(botID) + ". :calendar:")
-rtm_client.start()
+@slack_events_adapter.on('team_join')
+def greeting():
+    botID = 'U0133U2QXCL'
+    client.chat_postMessage(channel="general",
+                            text="Hello, this is your " + get_mention(botID) + ". :calendar:")
 
 
 # Slash commands
+@app.route('/event', methods=['POST'])
+def event_handler():
+    client.chat_postMessage(
+        channel='general',
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "What would you like to do?"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Make New Event",
+                            "emoji": True
+                        },
+                        "value": "new_event"
+                    }
+                ]
+            }
+        ]
+    )
+    return ''
 
-"""
-@post('/event')
-def event():
-    return "Would you like to create an event?"
-"""
-@route('/event',method="post")
-def event():
-    postdata = request.forms.get("text")
-    output_path = str("sndwserv:/" + parse.quote(postdata))
-    package = {"response_type": "in_channel", "text": "{}".format(output_path)}
-    response.content_type = 'application/json'
-    return package
+
+# Real time events
+@app.route('/slack/actions', methods=['POST'])
+def action_handler():
+    msg_action = json.loads(request.form["payload"])
+    actions = msg_action.get("actions")[0]
+    if msg_action.get("type") == "block_actions" and 'Make New Event' in actions['text']['text']:
+        client.views_open(
+            trigger_id=msg_action["trigger_id"],
+            view={
+                "type": "modal",
+                "callback_id": "modal-id",
+                "title": {
+                    "type": "plain_text",
+                    "text": "Create a New Event"
+                },
+                "submit": {
+                    "type": "plain_text",
+                    "text": "Submit"
+                },
+                "close": {
+                    "type": "plain_text",
+                    "text": "Cancel"
+                },
+                "blocks": [
+                    {
+                        "type": "input",
+                        "element": {
+                            "type": "datepicker",
+                            "initial_date": "1990-4-28",
+                            "placeholder": {
+                                "type": "plain_text",
+                                "text": "Select a Date",
+                                "emoji": True
+                            }
+                        },
+                        "label": {
+                            "type": "plain_text",
+                            "text": "Set Date",
+                            "emoji": True
+                        }
+
+                    }
+                ]
+            }
+        )
+        # client.chat_update(channel=cid, text="Creating your event...")
+    return make_response("", 200)
+
+
+# Prompts user when @ed
+@slack_events_adapter.on('app_mention')
+def ask(payload):
+    event = payload.get('event', {})
+    user_id = event.get("user")
+    cid = event.get('channel')
+    client.chat_postMessage(channel=cid, text="What can I do for you, " + get_mention(user_id) + "?")
 
 
 if __name__ == "__main__":
-    port_config = int(os.getenv('PORT', 3000))
-    run(host='0.0.0.0', port=port_config)
+    app.run(port=5000)
